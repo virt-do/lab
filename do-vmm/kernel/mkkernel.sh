1eeb353a204253ff0d88f6a31fb9a7956b9a4529
Samuel Ortiz <s.ortiz@apple.com>
2021-10-29 20:59:15 +0200

    do-vmm: Add kernel config and build script
    Signed-off-by: Samuel Ortiz <s.ortiz@apple.com>

---
+++ do-vmm/kernel/mkkernel.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+LINUX_REPO=linux-cloud-hypervisor
+
+if [ ! -d $LINUX_REPO ]
+then
+    git clone --depth 1 "https://github.com/cloud-hypervisor/linux.git" -b "ch-5.14" $LINUX_REPO
+fi
+
+pushd $LINUX_REPO
+cp ../linux-config-x86_64 .config
+make bzImage -j `nproc`
+popd
