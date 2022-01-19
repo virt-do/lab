e8a712cc3680d4b941c25a5f33f78848fa276414
thomasgouveia <programmationthomas@gmail.com>
2022-01-19 13:49:53 +0100

    do-vmm: update `mkbundle.sh` to take an argument for the bundle path location

--- do-vmm/rootfs/mkbundle.sh
+++ do-vmm/rootfs/mkbundle.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/bash
 
-mkdir -p ctr-bundle/rootfs
+DEST=${1:-"ctr-bundle"}
+
+mkdir -p "$DEST"/rootfs
 
 # Download and untar an alpine image
 curl -O https://dl-cdn.alpinelinux.org/alpine/v3.14/releases/x86_64/alpine-minirootfs-3.14.2-x86_64.tar.gz
-tar xf alpine-minirootfs-3.14.2-x86_64.tar.gz -C ctr-bundle/rootfs
+tar xf alpine-minirootfs-3.14.2-x86_64.tar.gz -C "$DEST"/rootfs
 
 # Create an ubuntu based root filesystem
 #podman export $(podman create ubuntu) | tar -C rootfs -xvf -
 
-pushd ctr-bundle
+pushd "$DEST"
 
 # Generate a runtime spec
 runc spec --rootless
