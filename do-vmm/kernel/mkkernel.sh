0ea7540a9b51f333cfa62b5a06910d9aca3297b6
Samuel Ortiz <sameo@rivosinc.com>
2022-10-09 20:59:32 +0200

    kernel: Update the build script
    Signed-off-by: Samuel Ortiz <sameo@rivosinc.com>

--- do-vmm/kernel/mkkernel.sh
+++ do-vmm/kernel/mkkernel.sh
@@ -4,10 +4,10 @@ LINUX_REPO=linux-cloud-hypervisor
 
 if [ ! -d $LINUX_REPO ]
 then
-    git clone --depth 1 "https://github.com/cloud-hypervisor/linux.git" -b "ch-5.14" $LINUX_REPO
+    git clone --depth 1 "https://github.com/cloud-hypervisor/linux.git" -b "ch-5.18.8" $LINUX_REPO
 fi
 
 pushd $LINUX_REPO
 cp ../linux-config-x86_64 .config
-make bzImage -j `nproc`
+KCFLAGS="-Wa,-mx86-used-note=no" make bzImage -j `nproc`
 popd
