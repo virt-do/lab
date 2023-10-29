2cb0bfdf7a23d00025166ba7f788521b9eac52c7
Samuel Ortiz <sameo@rivosinc.com>
2023-10-29 22:01:24 +0100

    do-vmm: Update kernel to 6.2
    Signed-off-by: Samuel Ortiz <sameo@rivosinc.com>

--- do-vmm/kernel/mkkernel.sh
+++ do-vmm/kernel/mkkernel.sh
@@ -4,7 +4,7 @@ LINUX_REPO=linux-cloud-hypervisor
 
 if [ ! -d $LINUX_REPO ]
 then
-    git clone --depth 1 "https://github.com/cloud-hypervisor/linux.git" -b "ch-5.18.8" $LINUX_REPO
+    git clone --depth 1 "https://github.com/cloud-hypervisor/linux.git" -b "ch-6.2" $LINUX_REPO
 fi
 
 pushd $LINUX_REPO
